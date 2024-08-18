e3769eb8d1a6f80b4d03032ec3c46c9e0581d6e1
Feryel1 <feriel.barira.etudiante@gmail.com>
2024-08-18 10:16:57 +0100

    Set up basic project structure for MERN social media app

---
+++ front/src/components/svgs/T.jsx
@@ -0,0 +1,13 @@
+const TSvg = (props) => (
+    <svg aria-hidden="true" viewBox="0 0 24 24" {...props}>
+      <path d="M3 2.25h18v2.5h-7.25v17h-3.5v-17H3v-2.5z" />
+      <path d="M5 4.75h14v1h-6.75v15h-0.5v-15H5v-1z" fill="none" />
+      <path d="M12 4.75v17" stroke="#000" strokeWidth="1" />
+      <path d="M8.5 7.75h7" stroke="#000" strokeWidth="1" />
+      <path d="M8.5 10.75h7" stroke="#000" strokeWidth="1" />
+      <path d="M8.5 13.75h7" stroke="#000" strokeWidth="1" />
+      <path d="M8.5 16.75h7" stroke="#000" strokeWidth="1" />
+    </svg>
+  );
+  export default TSvg;
+  
